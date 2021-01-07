18dc7f9684837638ffb1e046be034a18fc13591a
MarlboroYang <yangmarlboro@gmail.com>
2021-01-08 00:34:50 +0800

    fix the written

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    callback_for(:facebook)
+  end
 
-    def facebook
-      callback_for(:facebook)
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  def callback_for(provider)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url, alert: "需要您同意 Email 授權唷！"
     end
-  
-    def google_oauth2
-      callback_for(:google)
-    end
-  
-    def callback_for(provider)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-      else
-        session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-        redirect_to new_user_registration_url, alert: "需要您同意 Email 授權唷！"
-      end
-    end
-  
-    def failure
-      redirect_to root_path, alert: "無法獲得驗證！"
-    end
+  end
+
+  def failure
+    redirect_to root_path, alert: "無法獲得驗證！"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { 
+    :omniauth_callbacks => "users/omniauth_callbacks",
+     sessions: 'users/sessions',  
+     registrations: 'users/registrations'}
   root 'welcome#index'
   resources :boards do
     resources :posts, shallow: true
